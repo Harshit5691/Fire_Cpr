1c5ed2e89eac9b6c68b3f409964ccdb74c67939a
Ansuman-Hemant <ansuman.mitmpl2022@learner.manipal.edu>
2023-09-08 18:50:02 +0530

    Animations

---
+++ Cutscene street/Assets/Scripts/wayPoints.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wayPoints : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        foreach(Transform t in transform)
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(t.position, 1f);
+        }
+            
+        Gizmos.color = Color.red;
+        for (int i = 0; i < transform.childCount - 1; i++)
+        {
+            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i + 1).position);
+        }
+    }
+
+    public Transform GetNextWayPoint(Transform currentWaypoint)
+    {
+        if (currentWaypoint == null)
+        {
+            return transform.GetChild(0);
+        }
+
+        if(currentWaypoint.GetSiblingIndex() < transform.childCount - 1)
+        {
+            return transform.GetChild(currentWaypoint.GetSiblingIndex() + 1);
+        }
+        else
+        {
+            return transform.GetChild(0);
+        }
+    }
+}
---
+++ Cutscene street/Assets/Scripts/waypointMover.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypointMover : MonoBehaviour
+{
+    // Stores a reference to the waypoint system this object will use
+    [SerializeField] private wayPoints waypoints;
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    [SerializeField] private float distanceThreshold = 0.1f;
+
+    // The current waypoint target that the object is moving towards
+    private Transform currentWaypoint;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set initial position to the first waypoint
+        currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+
+
+        // Set the next waypoint target
+        currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+        transform.LookAt(currentWaypoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
+        {
+            currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+            transform.LookAt(currentWaypoint);
+        }
+    }
+}
